36b897a3840f62bd8909f1797197992cde548ba3
Chris Gomez <christiangomezdev@gmail.com>
2024-10-23 00:30:58 +0800

    chore(ui): Apply minor UI tweaks and adjustments
    This commit includes small UI fixes and adjustments to improve the user interface, based on recent feedback and design consistency checks.
    
    Closes #14

--- src/app/components/Header/Navbar/index.js
+++ src/app/components/Header/Navbar/index.js
@@ -1,22 +1,18 @@
 import Image from "next/image";
 
 const NavBar = () => {
-    return (
-        <div className="max-w-[1200px] mx-auto flex justify-between mt-[45px]">
-            <div className="text-left text-brand600 font-bold text-[28px] tracking-[0.1em]">
-                Travel
-            </div>
-            <div className="text-right">
-                <Image
-                    src="/menu.svg"
-                    alt="Menu"
-                    width={28}
-                    height={33}
-                    priority
-                />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="max-w-[1200px] mx-auto flex justify-between mt-[45px]">
+      <div className="text-left text-brand600 font-bold text-[28px] tracking-[0.1em]">
+        Travel
+      </div>
+      <div className="text-right">
+        <a href="#">
+          <Image src="/menu.svg" alt="Menu" width={28} height={33} priority />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
--- src/app/components/WhyChooseUs/Card/index.js
+++ src/app/components/WhyChooseUs/Card/index.js
@@ -1,23 +1,23 @@
 import { ArrowRight } from "iconsax-react";
 
-const Card = () => {
+const Card = ({isDark}) => {
   return (
-    <div className="flex mt-[60px] px-[32px] py-[40px] shadow-[10px_26px_150px_rgba(67,67,100,0.1)] border-t-[4px] border-t-[#25A59E]">
+    <div className={`flex mt-[60px] px-[32px] py-[40px] shadow-[10px_26px_150px_rgba(67,67,100,0.1)] border-t-[4px] border-t-[#25A59E] ${isDark ?  'bg-brand600' : ''}`}>
       <div className="flex flex-col">
         <div>
-          <div className="text-neutral900 text-heading-desktop-2 whitespace-pre-line">
+          <div className={`${isDark ?  'text-white' : 'text-neutral900'} text-heading-desktop-2 whitespace-pre-line font-gilda`}>
             {"Best Guide, Best\nExperience."}
           </div>
-          <div className="text-neutral600 text-[18px] font-urbanist mt-[20px] whitespace-pre-line">
+          <div className={`${isDark ?  'text-white' : 'text-neutral600'} text-[18px] font-urbanist mt-[20px] whitespace-pre-line`}>
             {"Your trip will be very memorable with\n the best guide."}
           </div>
         </div>
         <div className="flex mt-[80px]">
-          <div className="text-[20px] font-gilda text-brand600 flex ml-auto items-center">
+          <div className={`text-[20px] font-gilda ${isDark ?  'text-white' : 'text-brand600'} flex ml-auto items-center`}>
             <div className="mr-[12px]">
-              <a href="#">More details</a>
+              <a href="#" className="font-gilda">More details</a>
             </div>
-            <ArrowRight size={24} color="#25a59e" variant="Outline" />
+            <ArrowRight size={24} color={`${isDark ?  '#FFFFFF' : '#25a59e'}`} variant="Outline" />
           </div>
         </div>
       </div>
